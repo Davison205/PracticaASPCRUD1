2bb969fe61b569e245ab4aca3d2320b9dbff6670
Davison <davisontobon2019@gmail.com>
2021-03-16 23:59:29 -0500

    Se agrega la clase Empleado

---
+++ PracticaASPCRUD1/Models/Entities/Empleado.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PracticaASPCRUD1.Models.Entities
+{
+    public class Empleado
+    {
+        [Key]
+        public int EmpleadoId { get; set; }
+        [Column("NombreEmpleado", TypeName = "nvarchar(50)")]
+        [Required(ErrorMessage ="El nombre es requerido")]
+        public String Nombre { get; set; }
+        public int Documento { get; set; }
+        public int CargoId { get; set; }
+        public String Telefono { get; set; }
+
+
+
+    }
+}
